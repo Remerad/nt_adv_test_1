045231f88d535088b861d4526b7336607e9dfcf2
Remerad <jgodgiven@gmail.com>
2021-11-19 18:02:38 +0300

    начало

---
+++ test.py
@@ -0,0 +1,11 @@
+from main import*
+
+
+def test_check_document_existance(user_doc_number):
+    doc_founded = False
+    for current_document in documents:
+        doc_number = current_document['number']
+        if doc_number == user_doc_number:
+            doc_founded = True
+            break
+    return doc_founded
